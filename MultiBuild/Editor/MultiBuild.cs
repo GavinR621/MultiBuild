13b6a87a6e08fe086117d85d5c4d4948b4371710
GavinInfuseFlow <110934076+GavinInfuseFlow@users.noreply.github.com>
2022-11-14 17:24:09 -0500

    Bug Fix

--- MultiBuild/Editor/MultiBuild.cs
+++ MultiBuild/Editor/MultiBuild.cs
@@ -38,25 +38,21 @@ public class MultiBuild : EditorWindow
     Dictionary<BuildTarget, bool> TargetsToBuild = new Dictionary<BuildTarget, bool>();
     List<BuildTarget> AvailableTargets = new List<BuildTarget>();
 
-    void OnEnable()
+    private void OnEnable()
     {
+        AvailableTargets.Clear();
         var buildTargets = System.Enum.GetValues(typeof(BuildTarget));
-
         foreach (var buildTargetValue in buildTargets)
         {
             BuildTarget target = (BuildTarget)buildTargetValue;
 
             if (!BuildPipeline.IsBuildTargetSupported(GetTargetGroupForTarget(target), target))
-            {
                 continue;
-            }
 
             AvailableTargets.Add(target);
 
             if (!TargetsToBuild.ContainsKey(target))
-            {
                 TargetsToBuild[target] = false;
-            }
         }
 
         if (TargetsToBuild.Count > AvailableTargets.Count)
@@ -65,32 +61,25 @@ public class MultiBuild : EditorWindow
             foreach (var target in TargetsToBuild.Keys)
             {
                 if (!AvailableTargets.Contains(target))
-                {
                     targetsToRemove.Add(target);
-                }
             }
 
             foreach (var target in targetsToRemove)
-            {
                 TargetsToBuild.Remove(target);
-            }
         }
     }
 
-    void OnGUI()
+    private void OnGUI()
     {
         GUILayout.Label("Platforms to Build", EditorStyles.boldLabel);
 
         int numEnabled = 0;
-
         foreach (var target in AvailableTargets)
         {
             TargetsToBuild[target] = EditorGUILayout.Toggle(target.ToString(), TargetsToBuild[target]);
 
             if (TargetsToBuild[target])
-            {
                 numEnabled++;
-            }
         }
 
         if (numEnabled > 0)
@@ -98,47 +87,41 @@ public class MultiBuild : EditorWindow
             string prompt = numEnabled == 1 ? "Build 1 Platform" : $"Build {numEnabled} Platforms";
             if (GUILayout.Button(prompt))
             {
-                List<BuildTarget> selectedPlatforms = new List<BuildTarget>();
-
-                foreach (var target in selectedPlatforms)
+                List<BuildTarget> selectedTargets = new List<BuildTarget>();
+                foreach (var target in AvailableTargets)
                 {
                     if (TargetsToBuild[target])
-                    {
-                        selectedPlatforms.Add(target);
-                    }
+                        selectedTargets.Add(target);
                 }
 
-                EditorCoroutineUtility.StartCoroutine(PerformBuild(selectedPlatforms), this);
+                EditorCoroutineUtility.StartCoroutine(PerformBuild(selectedTargets), this);
             }
         }
     }
 
-    IEnumerator PerformBuild(List<BuildTarget> platformsToBuild)
+    IEnumerator PerformBuild(List<BuildTarget> targetsToBuild)
     {
         int buildAllProgressID = Progress.Start("Build All", "Building all selected platforms", Progress.Options.Sticky);
         Progress.ShowDetails();
-
         yield return new EditorWaitForSeconds(1f);
 
         BuildTarget originalTarget = EditorUserBuildSettings.activeBuildTarget;
 
-        for (int targetIndex = 0; targetIndex < TargetsToBuild.Count; ++targetIndex)
+        for (int targetIndex = 0; targetIndex < targetsToBuild.Count; ++targetIndex)
         {
-            var buildTarget = platformsToBuild[targetIndex];
+            var buildTarget = targetsToBuild[targetIndex];
 
+            Progress.Report(buildAllProgressID, targetIndex + 1, targetsToBuild.Count);
             int buildTaskProgressID = Progress.Start($"Build {buildTarget.ToString()}", null, Progress.Options.Sticky, buildAllProgressID);
-
             yield return new EditorWaitForSeconds(1f);
 
-            if (!BuildIndiviudalTarget(buildTarget))
+            if (!BuildIndividualTarget(buildTarget))
             {
                 Progress.Finish(buildTaskProgressID, Progress.Status.Failed);
                 Progress.Finish(buildAllProgressID, Progress.Status.Failed);
 
                 if (EditorUserBuildSettings.activeBuildTarget != originalTarget)
-                {
                     EditorUserBuildSettings.SwitchActiveBuildTargetAsync(GetTargetGroupForTarget(originalTarget), originalTarget);
-                }
 
                 yield break;
             }
@@ -149,54 +132,55 @@ public class MultiBuild : EditorWindow
 
         Progress.Finish(buildAllProgressID, Progress.Status.Succeeded);
 
+        if (EditorUserBuildSettings.activeBuildTarget != originalTarget)
+            EditorUserBuildSettings.SwitchActiveBuildTargetAsync(GetTargetGroupForTarget(originalTarget), originalTarget);
+
         yield return null;
     }
 
-    bool BuildIndiviudalTarget(BuildTarget target)
+    bool BuildIndividualTarget(BuildTarget target)
     {
         BuildPlayerOptions options = new BuildPlayerOptions();
 
         List<string> scenes = new List<string>();
-
         foreach (var scene in EditorBuildSettings.scenes)
-        {
             scenes.Add(scene.path);
-        }
 
         options.scenes = scenes.ToArray();
         options.target = target;
         options.targetGroup = GetTargetGroupForTarget(target);
-        
+
         if (target == BuildTarget.Android)
         {
             string apkName = PlayerSettings.productName + ".apk";
             options.locationPathName = System.IO.Path.Combine("Builds", target.ToString(), apkName);
         }
-        else
+        else if (target == BuildTarget.StandaloneWindows64)
         {
-            options.locationPathName = System.IO.Path.Combine("Builds", target.ToString(), PlayerSettings.productName);
+            options.locationPathName = System.IO.Path.Combine("Builds", target.ToString(), PlayerSettings.productName + ".exe");
         }
+        else if (target == BuildTarget.StandaloneLinux64)
+        {
+            options.locationPathName = System.IO.Path.Combine("Builds", target.ToString(), PlayerSettings.productName + ".x86_64");
+        }
+        else
+            options.locationPathName = System.IO.Path.Combine("Builds", target.ToString(), PlayerSettings.productName);
 
         if (BuildPipeline.BuildCanBeAppended(target, options.locationPathName) == CanAppendBuild.Yes)
-        {
             options.options = BuildOptions.AcceptExternalModificationsToPlayer;
-        }
         else
-        {
             options.options = BuildOptions.None;
-        }
 
         BuildReport report = BuildPipeline.BuildPlayer(options);
 
         if (report.summary.result == BuildResult.Succeeded)
         {
-            Debug.Log($"Build for {target.ToString()} completed in {report.summary.totalTime.TotalSeconds} seconds");
+            Debug.Log($"Build for {target.ToString()} completed in {report.summary.totalTime.Seconds} seconds");
             return true;
         }
-        else
-        {
-            Debug.LogError($"Build for {target.ToString()} failed");
-            return false;
-        }
+
+        Debug.LogError($"Build for {target.ToString()} failed");
+
+        return false;
     }
 }
